fc8a34c5c2157ea101cd19d340b37fd48c8c8abf
Md. Elias <elias1435@gmail.com>
2025-04-04 14:51:42 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,32 +1,4 @@
 <?php
-/*This file is part of ColorMagChild, colormag child theme.
-
-All functions of this file will be loaded before of parent theme functions.
-Learn more at https://codex.wordpress.org/Child_Themes.
-
-Note: this function loads the parent stylesheet before, then child theme stylesheet
-(leave it in place unless you know what you are doing.)
-*/
-
-if ( ! function_exists( 'suffice_child_enqueue_child_styles' ) ) {
-	function ColorMagChild_enqueue_child_styles() {
-	    // loading parent style
-	    wp_register_style(
-	      'parente2-style',
-	      get_template_directory_uri() . '/style.css'
-	    );
-
-	    wp_enqueue_style( 'parente2-style' );
-	    // loading child style
-	    wp_register_style(
-	      'childe2-style',
-	      get_stylesheet_directory_uri() . '/style.css'
-	    );
-	    wp_enqueue_style( 'childe2-style');
-	 }
-}
-add_action( 'wp_enqueue_scripts', 'ColorMagChild_enqueue_child_styles' );
-
 
 // add script in footer
 add_action('wp_footer', 'md_add_script_wp_footer');
